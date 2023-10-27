baa1ab6d131b0a9c8ec77a1e277f91c82bd88231
Josh <joshdebruxelles@gmail.com>
2023-10-27 11:57:59 +0100

    Updated Person.java and PersonTest.java tests created. Object count test failing.

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,18 +1,23 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.util.ArrayList;
 
 
 public class Person {
     String fName;
     String lName;
     LocalDate DOB;
-    public static int numberOfPeople=0;
+
+    public static ArrayList<Person> createdPeople = new ArrayList<Person>();
+    public Person () {
+
+    }
 
     public Person(String fName, String lName, LocalDate DOB) {
         this.fName = fName;
         this.lName = lName;
         this.DOB = DOB;
-        numberOfPeople++;
+        createdPeople.add(this);
     }
 
     public String getfName() {
@@ -53,17 +58,6 @@ public class Person {
         return "First Name: " + this.fName + " | Last Name: " + this.lName + " | DOB: " + this.DOB + " | Age: " + this.getAge();
     }
 
-    public static void main(String[] args) {
-        Person josh = new Person("Josh", "DB", LocalDate.parse("2002-03-11"));
-        Person dave = new Person("Dave", "Smith", LocalDate.parse("1999-04-12"));
-        Person bob = new Person("Bob", "Simpson", LocalDate.parse("2000-09-16"));
-        Person alice = new Person("Alice", "Inwonderland", LocalDate.parse("1980-01-31"));
-        System.out.println(josh);
-        System.out.println(dave);
-        System.out.println(bob);
-        System.out.println(alice);
-        System.out.println("Number of people: " + Person.numberOfPeople);
-    }
 }
 
 
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,3 +1,73 @@
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.*;
+
 public class PersonTest {
+
+//    @Test
+//    public void testObjectCounter(){
+//        Person josh = new Person("Josh", "DB", LocalDate.parse("2002-03-11"));
+//        Person dave = new Person("Dave", "Smith", LocalDate.parse("1999-04-12"));
+//        Person bob = new Person("Bob", "Simpson", LocalDate.parse("2000-09-16"));
+//        Person alice = new Person("Alice", "Inwonderland", LocalDate.parse("1980-01-31"));
+//        System.out.println(Person.createdPeople);
+//        assertEquals(4, Person.createdPeople.size());
+//    }
+
+    @Test
+    public void testGetfName() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        assertEquals("Josh",testPerson.getfName());
+    }
+
+    @Test
+    public void testSetfName() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        testPerson.setfName("Connor");
+        assertEquals("Connor",testPerson.getfName());
+    }
+
+    @Test
+    public void testGetlName() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        assertEquals("DB",testPerson.getlName());
+    }
+
+    @Test
+    public void testSetlName() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        testPerson.setlName("Mackintosh");
+        assertEquals("Mackintosh",testPerson.getlName());
+    }
+
+    @Test
+    public void testGetDOB() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        assertEquals(LocalDate.parse("2002-03-11"),testPerson.getDOB());
+    }
+
+    @Test
+    public void testSetDOB() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        testPerson.setDOB(LocalDate.parse("1995-10-29"));
+        assertEquals(LocalDate.parse("1995-10-29"),testPerson.getDOB());
+    }
+
+    @Test
+    public void testGetAge() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        int expectedAge = 21;
+        assertEquals(expectedAge,testPerson.getAge());
+    }
+
+    @Test
+    public void testToString() {
+        Person testPerson = new Person("Josh","DB", LocalDate.parse("2002-03-11"));
+        String expectedString = "First Name: " + "Josh" + " | Last Name: " + "DB" + " | DOB: " + LocalDate.parse("2002-03-11") + " | Age: " + 21;
+        assertEquals(expectedString, testPerson.toString());
+    }
+
 
 }
